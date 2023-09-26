4851895095a1676f697413bac8e0b7c3fcad4ee0
Ignatox <ignaciodiaz93@gmail.com>
2023-09-26 18:32:47 -0300

    Tptypsecript

---
+++ TPTypeScript/index.js
@@ -0,0 +1,15 @@
+fetch('https://fakestoreapi.com/products')
+.then(function (res) {return res.json();})
+.then(function (products){
+    var tableHTML = '<thead><tr><th>ID</th><th>Title</th><th>Description</th><th>Price</th></tr></thead><tbody>';
+    products.forEach(function (p){
+        tableHTML += "<tr>td>".concat(p.id, "</td><td>").concat(p.title, "</td><td>").concat(p.description, "</td><td>").concat(p.price, "</td><td>")
+    });
+
+    tableHTML += '</tbody>';
+
+    document.querySelector('#tableElement').innerHTML = tableHTML;
+
+    var spinnerElement = docuiment.querySelector('#SpinnerContainer');
+    spinnerElement.style.display='none';
+});
--- TPTypeScript/index.ts
+++ TPTypeScript/index.ts
@@ -1,8 +1,31 @@
-type Product{
+type Rating = {
+    count: number;
+    rate: number;
+    };
+    type Product = {
+    category: string;
     description: string;
     id: number;
     image: string;
     price: number;
     rating: Rating;
     title: string;
-}
+    };
+    fetch('https://fakestoreapi.com/products')
+    .then(res => res.json())
+    .then((products: Product[]) => {
+    // Tabla HTML preparacion
+    let tableHTML: string = '<thead><tr><th>ID</th><th>Title</th><th>Description</th><th>Price</th></tr></thead><tbody>';
+    // Genero los productos en loop para la tabla
+    products.forEach((p: Product) => {
+    tableHTML += `<tr><td>${p.id}</td><td>${p.title}</td><td>${p.description}</td><td>${p.price}</td></tr>`;
+    });
+    // Cierro body de tabla
+    tableHTML += '</tbody>';
+    // Grab table element to set its inner HTML
+    document.querySelector('#tableElement')!.innerHTML = tableHTML; //fuerzo a que no sea opcional
+    // Hide spinner
+    const spinnerElement: HTMLElement = document.querySelector('#spinnerContainer')!;
+    spinnerElement.style.display = 'none';
+    });
+    

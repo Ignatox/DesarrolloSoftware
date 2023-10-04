6e93beb8aa123b565c9cc2a40730896eb6513b82
Ignatox <ignaciodiaz93@gmail.com>
2023-10-03 21:44:45 -0300

    Final, prueba de datos y collection.josn

--- TP-APIREST-Diaz/src/main/java/com/utn/TPAPIRESTDiaz/TpApirestDiazApplication.java
+++ TP-APIREST-Diaz/src/main/java/com/utn/TPAPIRESTDiaz/TpApirestDiazApplication.java
@@ -1,7 +1,13 @@
 package com.utn.TPAPIRESTDiaz;
 
+import com.utn.TPAPIRESTDiaz.entities.Localidad;
+import com.utn.TPAPIRESTDiaz.repositories.LocalidadRepository;
+import com.utn.TPAPIRESTDiaz.repositories.PersonaRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cglib.core.Local;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TpApirestDiazApplication {
